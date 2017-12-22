0460d7c8d8396e98c5be62507f7da7c38df66f2a
Brian Payne <brianpayne@brians-mbp.attlocal.net>
2017-12-22 13:18:56 -0600

    Added URL parameterization

--- Dead_Link_Scraper.py
+++ Dead_Link_Scraper.py
@@ -3,10 +3,22 @@ import requests
 from lxml import html
 from bs4 import BeautifulSoup
 
+# Usage
+if len(sys.argv) == 1:
+	print("Please input a URL in the format of: http://www.your_url_here.com")
+	sys.exit(2)
+
 # Globals
 link_list = []
 bad_links = []
-r = requests.get("http://www.monetate.com/")
+base_url = str(sys.argv[1])
+
+# Print executing info
+print("This is the name of the script: ", sys.argv[0])
+print("The website to test is: " , base_url)
+
+# Prepare request
+r = requests.get(str(base_url))
 soup = BeautifulSoup(r.content, "html.parser")
 
 # Iterate through Monetate HTML to get link_list of all valid hyperlinks
@@ -17,14 +29,11 @@ for a in soup.findAll("a", href=True):
 # Iterate through link_list
 for link in link_list:
 	# Scrub URLs
-	if link[0] == '/' or link[0] == '#':
-		link = 'https://www.monetate.com' + link
-	if link[:3] == 'tel':
-		continue
-	
-	link_request = requests.get(link)
-	if str(link_request.status_code)[0] == '4':
-		bad_links.append(link)
+	if link[:4] == 'http':
+		#print(link)
+		link_request = requests.get(link)
+		if str(link_request.status_code)[0] == '2':
+			bad_links.append(link)
 
 # Print output to console
 print(bad_links)
